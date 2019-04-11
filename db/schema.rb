6ece433d1313c5660cdc963e32431cbd2e978b1c
furu-a <croqkes@gmail.com>
2019-04-11 14:42:23 +0900

    furu-a_header

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_06_074739) do
+ActiveRecord::Schema.define(version: 2019_04_09_061733) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -88,6 +88,8 @@ ActiveRecord::Schema.define(version: 2019_04_06_074739) do
     t.integer "saler_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "images"
   end
 
   create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
