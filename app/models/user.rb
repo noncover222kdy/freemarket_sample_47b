02eefd8d2037de4aae7d7c26419bc9423dce2344
furu-a <croqkes@gmail.com>
2019-04-13 06:16:22 +0900

    add_item_and_user_foreign-key

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable
+
   has_many :items
+
   has_many :works
   has_many :infos
   has_many :news
@@ -14,4 +16,6 @@ class User < ApplicationRecord
   has_many :addresses
   has_many :likes, dependent: :destroy
   has_many :likes_items, through: :likes, source: :item
+
+  has_many :items, class_name: 'Item', foreign_key: :saler_id , primary_key: :id
 end
