e12650a8146f555dcb48ec1cd8828b34449a3f4b
noncover222kdy <h.shareworld.y81d@icloud.com>
2019-04-12 14:24:52 +0900

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users
   root 'items#index'
 
   resources :items do
@@ -14,9 +14,6 @@ Rails.application.routes.draw do
     end
     resources :addresses, only: [:new, :create, :edit, :show, :update, :destroy]
     resources :banks, only: [:new, :create, :edit, :update, :destroy]
-    collection do
-      get 'select'
-    end
   end
   # get 'users/log_out'  => 'users#log_out', as: 'log_out_user'
   # get 'users/about'    => 'users#about',   as: 'about_user'
